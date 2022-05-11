9812e110736a99dd6672f1fbaa9df41987c0a589
Kamen Dimitrov <79080702+KamkomWork@users.noreply.github.com>
2022-05-11 14:32:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Summator
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+
+
+            Console.WriteLine("Enter input");
+            string input = Console.ReadLine();
+            int expectedValue = Int32.Parse(input);
+
+            if (result == expectedValue)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+
+            }
+
+
+            long average = Summator.Average(new int[] { 5, 10, 15, });
+
+            if (average == 10)
+            {
+                Console.WriteLine("TEST PASS");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAIL");
+
+            }
+
+
+        }
+           
+
+
+
+
+
+
+    }
+
+
+   
+
+
+
+
+
+}
